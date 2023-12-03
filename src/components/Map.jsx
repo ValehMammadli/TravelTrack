ca5b77a3f41ec038fc055aa4db8878970710c662
Valeh Mammadli <valehlothbrok@gmail.com>
2023-12-03 15:44:37 +0400

    Add Leaflet map functionality to Map component
    This commit introduces the Leaflet library to the Map component. The Leaflet library is used to display interactive maps and location data. The changes include:
    
    - Adding Leaflet and React-Leaflet dependencies to package.json and package-lock.json.
    - Updating the Map component to use Leaflet's MapContainer, TileLayer, Marker, Popup, useMap, and useMapEvents.
    - Adding a function to convert flag emojis to PNG images for display on the map.
    - Adding a function to change the center of the map based on the user's location.
    - Adding a function to detect clicks on the map and navigate to a form with the clicked location's latitude and longitude.
    - Removing the previous placeholder content in the Map component.
    
    This update enhances the user experience by providing a visual and interactive map for users to select locations.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,25 +1,83 @@
+import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 import styles from "./Map.module.css";
+import { useCities } from "../contexts/CitiesContext";
 
+const flagemojiToPNG = (flag) => {
+  var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+    .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+    .join("");
+  return (
+    <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+  );
+};
 function Map() {
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const [searchParams] = useSearchParams();
 
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
   return (
     <div
       className={styles.mapContainer}
-      onClick={() => {
-        navigate("form");
-      }}
+     
     >
-      <h1>Map</h1>
-      <h1>
-        Position: {lat},{lng}
-      </h1>
+      <MapContainer
+        center={mapPosition}
+        zoom={6}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            key={city.id}
+            position={[city.position.lat, city.position.lng]}
+          >
+            <Popup>
+              <span>{flagemojiToPNG(city.emoji)}</span>{" "}
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
+      </MapContainer>
     </div>
   );
 }
 
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+ 
+  useMapEvents({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
+}
 export default Map;
