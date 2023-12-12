ba19903d3377d3c7586524d277761dd92447ad57
Valeh Mammadli <valehlothbrok@gmail.com>
2023-12-12 19:02:02 +0400

    Adding Fake Authentication: Protecting a Route

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,14 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useEffect } from "react";
 import Map from "../components/Map"
 import Sidebar from "../components/Sidebar"
 import User from "../components/User"
 import styles from './AppLayout.module.css'
 
 function AppLayout() {
+  const {isAuthenticated} = useAuth();
+const navigate = useNavigate();
+
+useEffect(function(){
+    if (!isAuthenticated) 
+        navigate("/")
+    
+},[isAuthenticated,navigate])
   return (
     <div className={styles.app}>
        <Sidebar/>
        <Map/>
-       <User/>
+     {isAuthenticated && <User/>}  
         </div>
   )
 }
